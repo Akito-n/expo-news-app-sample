55c2ee641a38da629f3e4baa32462652f36a8677
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-10-18 17:08:18 +0900

    記事詳細スクリーンの雛形

---
+++ screens/ArticleScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+
+const ArticleScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>新しいスクリーン</View>
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+})
+
+export default ArticleScreen
