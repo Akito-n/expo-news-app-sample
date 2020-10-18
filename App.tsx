5b7332d9f21e4695886909054de7519891ef7c84
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-10-18 16:54:07 +0900

    screenにするのとnavigatorを入れてリファクタリングする

--- App.tsx
+++ App.tsx
@@ -1,53 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import React, {useEffect, useState} from 'react'
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
-import ListItem, { ContentProps } from './components/ListItem'
-import ListItemData from './dummy/listItemData'
-import {env} from './config/env'
-import axios from 'axios'
+import React from 'react'
+import AppNavigator from './navigation/AppNavigator';
 import './config/dayjs'
 
-const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${env.newsApi}`
 
 const App = () => {
-  const [articles, setArticles ] = useState<ContentProps[]>(ListItemData)
-  useEffect(() => {
-  fetchArticles()
-
-  }, [])
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL)
-      setArticles(() => (response.data.articles))
-    } catch (e) {
-      console.log(e)
-    }
-  }
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            title={item.title}
-            urlToImage={item.urlToImage}
-            author={item.author}
-            publishedAt={item.publishedAt}
-          />
-        )}
-        keyExtractor={(_, index) => index.toString()}
-      />
-    </SafeAreaView>
+    <AppNavigator />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
-})
 
 export default App
