f6b084c59d8bc30c606d6634a9f0b143b072fdce
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-10-25 15:00:50 +0900

    expo-iconを導入してそれっぽい見た目にしてみる

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,23 +1,60 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
-import ArticleScreen from '../screens/ArticleScreen';
-import { ArticleProp } from '../components/ListItem';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from '../screens/HomeScreen'
+import ArticleScreen from '../screens/ArticleScreen'
+import ClipScreen from '../screens/ClipScreen';
+import { ArticleProp } from '../components/ListItem'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesome } from '@expo/vector-icons'
+import { color } from 'react-native-reanimated'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const HomeStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}}/>
+      <Stack.Screen name="Article" component={ArticleScreen} options={{ headerShown: false}}/>
+    </Stack.Navigator>
+  )
+}
+
+const ClipStack = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Clip" component={ClipScreen} options={{ headerShown: false}}/>
+    </Stack.Navigator>
+  )
+}
 
 export type StackParamList = {
   Article: {article: ArticleProp}
 }
 
+const screenOption = ({route}) => ({
+  tabBarIcon: ({color, size}: {color: string, size: number}) => {
+    let iconName = 'home'
+    switch (route.name) {
+      case 'Home':
+        iconName = 'home';
+        break;
+      case 'Clip':
+        iconName = 'bookmark';
+        break;
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+});
+
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}}/>
-        <Stack.Screen name="Article" component={ArticleScreen} options={{ headerShown: false}}/>
-      </Stack.Navigator>
+     <Tab.Navigator screenOptions={screenOption}>
+       <Tab.Screen name="Top" component={HomeStack} />
+       <Tab.Screen name="Clip" component={ClipStack} />
+     </Tab.Navigator>
     </NavigationContainer>
   )
 }
